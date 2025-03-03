881a6695c6b93d77e94bb9bfd853c1b99079a2e9
Quackers29 <gazer29@hotmail.com>
2025-03-03 06:44:28 +0000

    Add to 3.1, tourist quit or die, town replenishes

---
+++ src/main/java/com/yourdomain/businesscraft/client/ClientSetup.java
@@ -0,0 +1,25 @@
+package com.yourdomain.businesscraft.client;
+
+import com.yourdomain.businesscraft.BusinessCraft;
+import com.yourdomain.businesscraft.client.renderer.TouristRenderer;
+import com.yourdomain.businesscraft.entity.ModEntityTypes;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = BusinessCraft.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientSetup {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        // Client-side setup code
+    }
+    
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        // Register the tourist renderer
+        event.registerEntityRenderer(ModEntityTypes.TOURIST.get(), TouristRenderer::new);
+    }
+} 
