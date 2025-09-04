2decd1637a24098f455c36afd4b97e85b301fe2a
Quackers29 <gazer29@hotmail.com>
2025-09-04 14:17:15 +0100

    Revert "Update TownInterfaceEntity.java"
    This reverts commit e470c09a2a2d3ca1deadc1d3708e385d2754e423.

--- forge/src/main/java/com/quackers29/businesscraft/block/entity/TownInterfaceEntity.java
+++ forge/src/main/java/com/quackers29/businesscraft/block/entity/TownInterfaceEntity.java
@@ -1,7 +1,6 @@
 package com.quackers29.businesscraft.block.entity;
 
 import com.quackers29.businesscraft.init.ModBlockEntities;
-import com.quackers29.businesscraft.town.TownInterfaceData;
 import com.quackers29.businesscraft.town.data.PlatformManager;
 import com.quackers29.businesscraft.town.data.TouristSpawningHelper;
 import com.quackers29.businesscraft.town.data.VisitorProcessingHelper;
@@ -169,34 +168,8 @@ public class TownInterfaceEntity extends com.quackers29.businesscraft.block.enti
     // Additional platform-specific method implementations
     @Override
     public Object getTownInterfaceData() {
-        // Create and return a TownInterfaceData instance for UI operations
-        TownInterfaceData data = new TownInterfaceData(
-            this.worldPosition.getX(),
-            this.worldPosition.getY(),
-            this.worldPosition.getZ()
-        );
-
-        // Copy relevant data from the entity to the TownInterfaceData
-        if (this.townId != null) {
-            data.setTownId(this.townId);
-        }
-        if (this.name != null) {
-            data.setTownName(this.name);
-        }
-        data.setTouristSpawningEnabled(this.touristSpawningEnabled);
-        data.setSearchRadius(this.searchRadius);
-
-        // Copy platforms if available
-        var platformManager = getPlatformManager();
-        if (platformManager != null) {
-            com.quackers29.businesscraft.town.data.PlatformManager pm =
-                (com.quackers29.businesscraft.town.data.PlatformManager) platformManager;
-            // Convert platform manager platforms to TownInterfaceData platforms
-            // Note: getPlatforms requires a boolean parameter (isClientSide)
-            // For now, we'll skip platform copying until proper mapping is implemented
-        }
-
-        return data;
+        // Return this entity as the town interface data
+        return this;
     }
 
     @Override
