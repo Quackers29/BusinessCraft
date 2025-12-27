818a564391e1030a23ee8168318c4e28528d2858
Quackers29 <gazer29@hotmail.com>
2025-12-27 05:32:06 +0000

    Reapply "GEM Test of production addition"
    This reverts commit 4c0f8b13f234805a5f5e976a1b36ab420b17bf9f.

--- common/src/main/java/com/quackers29/businesscraft/production/Upgrade.java
+++ common/src/main/java/com/quackers29/businesscraft/production/Upgrade.java
@@ -4,16 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Upgrade {
+    public enum UpgradeTrack {
+        RESOURCES, SERVICES, POPULATION
+    }
+
     private final String id;
     private final String name;
     private final int populationReq;
+    private final UpgradeTrack track;
+    private final int tier;
+    private final float happinessBonus;
+    private final int housingCapacity;
+
     private final Map<String, Float> inputRates = new HashMap<>();
     private final Map<String, Float> outputRates = new HashMap<>();
+    private final Map<String, Integer> buildCost = new HashMap<>();
 
-    public Upgrade(String id, String name, int populationReq) {
+    public Upgrade(String id, String name, int populationReq, UpgradeTrack track, int tier, float happinessBonus,
+            int housingCapacity) {
         this.id = id;
         this.name = name;
         this.populationReq = populationReq;
+        this.track = track;
+        this.tier = tier;
+        this.happinessBonus = happinessBonus;
+        this.housingCapacity = housingCapacity;
     }
 
     public String getId() {
@@ -28,6 +43,22 @@ public class Upgrade {
         return populationReq;
     }
 
+    public UpgradeTrack getTrack() {
+        return track;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public float getHappinessBonus() {
+        return happinessBonus;
+    }
+
+    public int getHousingCapacity() {
+        return housingCapacity;
+    }
+
     public Map<String, Float> getInputRates() {
         return inputRates;
     }
@@ -36,11 +67,19 @@ public class Upgrade {
         return outputRates;
     }
 
+    public Map<String, Integer> getBuildCost() {
+        return buildCost;
+    }
+
     public void addInput(String resourceId, float rate) {
         inputRates.put(resourceId, rate);
     }
 
     public void addOutput(String resourceId, float rate) {
         outputRates.put(resourceId, rate);
+    }
+
+    public void addBuildCost(String resourceId, int amount) {
+        buildCost.put(resourceId, amount);
     }
 }
